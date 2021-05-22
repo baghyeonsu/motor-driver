1673a2f0937a852afc7a4f153797173210d1282c
baghyeonsu <84246104+baghyeonsu@users.noreply.github.com>
2021-05-22 14:34:05 -0700

    add second motor control

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+# disable led screen so that p3 is available for motor control
+led.enable(False)
+
+def motorBCoast():
+    pins.digital_write_pin(DigitalPin.P2, 0)
+    pins.digital_write_pin(DigitalPin.P3, 0)
 def motorACoast():
     pins.digital_write_pin(DigitalPin.P0, 0)
     pins.digital_write_pin(DigitalPin.P1, 0)
 def setMotorASpeed(speed: number):
     if speed < 0:
-        pins.analog_write_pin(AnalogPin.P0, (0 - speed) * 10)
+        pins.analog_write_pin(AnalogPin.P0, -1 * speed * 10)
         pins.digital_write_pin(DigitalPin.P1, 0)
     elif speed == 0:
         motorACoast()
@@ -13,30 +19,40 @@ def setMotorASpeed(speed: number):
         pins.analog_write_pin(AnalogPin.P1, speed * 10)
 
 def on_button_pressed_a():
-    global Speed_Left
-    Speed_Left= +1
-
+    global Speed_Right
+    Speed_Right = Speed_Right + 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global speed
-    Speed_Right =+1
-    Speed_Left  =+1
-    pins.digital_write_pin(DigitalPin.P0, 1)
-    pins.digital_write_pin(DigitalPin.P1, 1)
+    global speed, Speed_Right, Speed_Left
+    speed = 0
+    Speed_Right = 0
+    Speed_Left = 0
+    motorACoast()
+    motorBCoast()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global Speed_Right
-    Speed_Right= +1
+    Speed_Right = Speed_Right - 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def setMotorBSpeed(speed: number):
+    if speed < 0:
+        pins.analog_write_pin(AnalogPin.P2, -1 * speed * 10)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+    elif speed == 0:
+        motorBCoast()
+    else:
+        # speed has to be positive to get here
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.analog_write_pin(AnalogPin.P3, speed * 10)
+Speed_Left = 0
 speed = 0
 Speed_Right = 0
-Speed_Left = 0
 motorACoast()
 
 def on_forever():
-    basic.show_number(speed)
-    setMotorASpeed(speed)
+    # basic.show_number(Speed_Right)
+    setMotorBSpeed(Speed_Right)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+//  disable led screen so that p3 is available for motor control
+led.enable(false)
+function motorBCoast() {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P3, 0)
+}
+
 function motorACoast() {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -5,7 +12,7 @@ function motorACoast() {
 
 function setMotorASpeed(speed: number) {
     if (speed < 0) {
-        pins.analogWritePin(AnalogPin.P0, (0 - speed) * 10)
+        pins.analogWritePin(AnalogPin.P0, -1 * speed * 10)
         pins.digitalWritePin(DigitalPin.P1, 0)
     } else if (speed == 0) {
         motorACoast()
@@ -19,24 +26,39 @@ function setMotorASpeed(speed: number) {
 
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    Speed_Left = +1
+    Speed_Right = Speed_Right + 1
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
-    let Speed_Right = +1
-    let Speed_Left = +1
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 1)
+    speed = 0
+    Speed_Right = 0
+    Speed_Left = 0
+    motorACoast()
+    motorBCoast()
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    Speed_Right = +1
+    Speed_Right = Speed_Right - 1
 })
+function setMotorBSpeed(speed: number) {
+    if (speed < 0) {
+        pins.analogWritePin(AnalogPin.P2, -1 * speed * 10)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+    } else if (speed == 0) {
+        motorBCoast()
+    } else {
+        //  speed has to be positive to get here
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P3, speed * 10)
+    }
+    
+}
+
+let Speed_Left = 0
 let speed = 0
 let Speed_Right = 0
-let Speed_Left = 0
 motorACoast()
 basic.forever(function on_forever() {
-    basic.showNumber(speed)
-    setMotorASpeed(speed)
+    //  basic.show_number(Speed_Right)
+    setMotorBSpeed(Speed_Right)
 })
