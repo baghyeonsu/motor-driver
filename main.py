186bfd432771553c462628bfb626593779530872
baghyeonsu <84246104+baghyeonsu@users.noreply.github.com>
2021-05-22 14:38:50 -0700

    tidy up

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# disable led screen so that p3 is available for motor control
-led.enable(False)
-
-def motorBCoast():
-    pins.digital_write_pin(DigitalPin.P2, 0)
-    pins.digital_write_pin(DigitalPin.P3, 0)
 def motorACoast():
     pins.digital_write_pin(DigitalPin.P0, 0)
     pins.digital_write_pin(DigitalPin.P1, 0)
+
 def setMotorASpeed(speed: number):
     if speed < 0:
         pins.analog_write_pin(AnalogPin.P0, -1 * speed * 10)
@@ -17,6 +12,21 @@ def setMotorASpeed(speed: number):
         # speed has to be positive to get here
         pins.digital_write_pin(DigitalPin.P0, 0)
         pins.analog_write_pin(AnalogPin.P1, speed * 10)
+
+def motorBCoast():
+    pins.digital_write_pin(DigitalPin.P2, 0)
+    pins.digital_write_pin(DigitalPin.P3, 0)
+
+def setMotorBSpeed(speed: number):
+    if speed < 0:
+        pins.analog_write_pin(AnalogPin.P2, -1 * speed * 10)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+    elif speed == 0:
+        motorBCoast()
+    else:
+        # speed has to be positive to get here
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.analog_write_pin(AnalogPin.P3, speed * 10)
 
 def on_button_pressed_a():
     global Speed_Right
@@ -37,20 +47,13 @@ def on_button_pressed_b():
     Speed_Right = Speed_Right - 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def setMotorBSpeed(speed: number):
-    if speed < 0:
-        pins.analog_write_pin(AnalogPin.P2, -1 * speed * 10)
-        pins.digital_write_pin(DigitalPin.P3, 0)
-    elif speed == 0:
-        motorBCoast()
-    else:
-        # speed has to be positive to get here
-        pins.digital_write_pin(DigitalPin.P2, 0)
-        pins.analog_write_pin(AnalogPin.P3, speed * 10)
 Speed_Left = 0
 speed = 0
 Speed_Right = 0
+# disable led screen so that p3 is available for motor control
+led.enable(False)
 motorACoast()
+motorBCoast()
 
 def on_forever():
     # basic.show_number(Speed_Right)
--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-//  disable led screen so that p3 is available for motor control
-led.enable(false)
-function motorBCoast() {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P3, 0)
-}
-
 function motorACoast() {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -20,6 +13,25 @@ function setMotorASpeed(speed: number) {
         //  speed has to be positive to get here
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.analogWritePin(AnalogPin.P1, speed * 10)
+    }
+    
+}
+
+function motorBCoast() {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P3, 0)
+}
+
+function setMotorBSpeed(speed: number) {
+    if (speed < 0) {
+        pins.analogWritePin(AnalogPin.P2, -1 * speed * 10)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+    } else if (speed == 0) {
+        motorBCoast()
+    } else {
+        //  speed has to be positive to get here
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P3, speed * 10)
     }
     
 }
@@ -40,24 +52,13 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     Speed_Right = Speed_Right - 1
 })
-function setMotorBSpeed(speed: number) {
-    if (speed < 0) {
-        pins.analogWritePin(AnalogPin.P2, -1 * speed * 10)
-        pins.digitalWritePin(DigitalPin.P3, 0)
-    } else if (speed == 0) {
-        motorBCoast()
-    } else {
-        //  speed has to be positive to get here
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, speed * 10)
-    }
-    
-}
-
 let Speed_Left = 0
 let speed = 0
 let Speed_Right = 0
+//  disable led screen so that p3 is available for motor control
+led.enable(false)
 motorACoast()
+motorBCoast()
 basic.forever(function on_forever() {
     //  basic.show_number(Speed_Right)
     setMotorBSpeed(Speed_Right)
