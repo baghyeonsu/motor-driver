7334c301610679dd5216c8729089c0fbcec56c78
baghyeonsu <84246104+baghyeonsu@users.noreply.github.com>
2021-05-29 11:59:58 -0700

    remote control

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+def motorBCoast():
+    pins.digital_write_pin(DigitalPin.P2, 0)
+    pins.digital_write_pin(DigitalPin.P3, 0)
 def motorACoast():
     pins.digital_write_pin(DigitalPin.P0, 0)
     pins.digital_write_pin(DigitalPin.P1, 0)
-
 def setMotorASpeed(speed: number):
     if speed < 0:
         pins.analog_write_pin(AnalogPin.P0, -1 * speed * 10)
@@ -13,26 +15,10 @@ def setMotorASpeed(speed: number):
         pins.digital_write_pin(DigitalPin.P0, 0)
         pins.analog_write_pin(AnalogPin.P1, speed * 10)
 
-def motorBCoast():
-    pins.digital_write_pin(DigitalPin.P2, 0)
-    pins.digital_write_pin(DigitalPin.P3, 0)
-
-def setMotorBSpeed(speed: number):
-    if speed < 0:
-        pins.analog_write_pin(AnalogPin.P2, -1 * speed * 10)
-        pins.digital_write_pin(DigitalPin.P3, 0)
-    elif speed == 0:
-        motorBCoast()
-    else:
-        # speed has to be positive to get here
-        pins.digital_write_pin(DigitalPin.P2, 0)
-        pins.analog_write_pin(AnalogPin.P3, speed * 10)
-
 def on_button_pressed_a():
-    global Speed_Right
-    global Speed_Left
+    global Speed_Right, Speed_Left
     Speed_Right = Speed_Right + 8
-    Speed_Left  = Speed_Left  + 10
+    Speed_Left = Speed_Left + 10
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
@@ -45,15 +31,34 @@ def on_button_pressed_ab():
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    global Speed_Right
-    global Speed_Left
+    global Speed_Right, Speed_Left
     Speed_Right = Speed_Right - 8
-    Speed_Left  = Speed_Left  - 10
+    Speed_Left = Speed_Left - 10
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-Speed_Left = 0
+def setMotorBSpeed(speed: number):
+    if speed < 0:
+        pins.analog_write_pin(AnalogPin.P2, -1 * speed * 10)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+    elif speed == 0:
+        motorBCoast()
+    else:
+        # speed has to be positive to get here
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        pins.analog_write_pin(AnalogPin.P3, speed * 10)
+
+def on_received_value(name, value):
+    global Speed_Left
+    if name == "speed_left":
+        Speed_Left = value * 1000
+    elif name == "speed_right":
+        Speed_Right=value *  1000
+radio.on_received_value(on_received_value)
+
 speed = 0
+Speed_Left = 0
 Speed_Right = 0
+radio.set_group(1)
 # disable led screen so that p3 is available for motor control
 led.enable(False)
 motorACoast()
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+function motorBCoast() {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P3, 0)
+}
+
 function motorACoast() {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -17,27 +22,7 @@ function setMotorASpeed(speed: number) {
     
 }
 
-function motorBCoast() {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P3, 0)
-}
-
-function setMotorBSpeed(speed: number) {
-    if (speed < 0) {
-        pins.analogWritePin(AnalogPin.P2, -1 * speed * 10)
-        pins.digitalWritePin(DigitalPin.P3, 0)
-    } else if (speed == 0) {
-        motorBCoast()
-    } else {
-        //  speed has to be positive to get here
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P3, speed * 10)
-    }
-    
-}
-
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
     
     Speed_Right = Speed_Right + 8
     Speed_Left = Speed_Left + 10
@@ -52,13 +37,37 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    
     Speed_Right = Speed_Right - 8
     Speed_Left = Speed_Left - 10
 })
-let Speed_Left = 0
+function setMotorBSpeed(speed: number) {
+    if (speed < 0) {
+        pins.analogWritePin(AnalogPin.P2, -1 * speed * 10)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+    } else if (speed == 0) {
+        motorBCoast()
+    } else {
+        //  speed has to be positive to get here
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P3, speed * 10)
+    }
+    
+}
+
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let Speed_Right: number;
+    
+    if (name == "speed_left") {
+        Speed_Left = value * 1000
+    } else if (name == "speed_right") {
+        Speed_Right = value * 1000
+    }
+    
+})
 let speed = 0
+let Speed_Left = 0
 let Speed_Right = 0
+radio.setGroup(1)
 //  disable led screen so that p3 is available for motor control
 led.enable(false)
 motorACoast()
